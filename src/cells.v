e6dba2e79b601ba995644e20286be1101fac29a2
DakotaThorpe <92582952+DakotaThorpe@users.noreply.github.com>
2023-03-27 11:49:43 -0300

    Update cells.v

--- src/cells.v
+++ src/cells.v
@@ -103,7 +103,7 @@ endmodule
 
 module counter (input clk,      // Declare input port for the clock to allow counter to count up  
                   input rstn,              // Declare input port for the reset to allow the counter to be reset to 0 when required  
-                  output reg[3:0] out);    // Declare 4-bit output port to get the counter values  
+                  output reg[63:0] out);    // Declare 4-bit output port to get the counter values  
   
  // This always block will be triggered at the rising edge of clk (0->1)  
   // Once inside this block, it checks if the reset is 0, then change out to zero   
